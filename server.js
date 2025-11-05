6e6b88073f554999f99f0dcf28172d40f3a818ed
yutai934357 <luffy20158413@gmail.com>
2025-11-05 16:02:17 +0800

    1105 結束

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 var express = require("express");
 var server = express();
+var bodyParser = require("body-parser");
 
-var bodyParser = require("body-parser");  
-server.use(express.static(__dirname+"/Public")); 
+server.use(express.static(__dirname+"/Public"));
 server.use(bodyParser.urlencoded());
+
+
+
 server.get("/", (req,res)=>{
     res.send("Hello world!");
 })
-
-server.get("/about", (req,res)=>{
-    res.send("My first NodeJS server!");
+server.get("/services", (req,res)=>{
+    //db 
+    var Services= [
+                {icon:'fa-shopping-cart', title:'E-Commerce', text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.'},
+                {icon:'fa-laptop', title:'Responsive Design', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+                {icon:'fa-lock', title:'Web Security', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'}
+            ]
+    res.send(Services);
 })
 
-server.listen(8080)
+server.get("/portfolio", (req,res)=>{
+
+})
+
+server.get("/about", (req,res)=>{
+    res.send("Welcome "+req.query.user+" to My first NodeJS server!");
+})
+
+server.listen(80)
