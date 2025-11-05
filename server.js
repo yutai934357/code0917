c1859f6990e7ed9fd8089fcc74f1a4bf1fd0db93
yutai934357 <luffy20158413@gmail.com>
2025-11-05 15:27:41 +0800

    110501

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var server = express();
+
+var bodyParser = require("body-parser");  
+server.use(express.static(__dirname+"/Public")); 
+server.use(bodyParser.urlencoded());
+server.get("/", (req,res)=>{
+    res.send("Hello world!");
+})
+
+server.get("/about", (req,res)=>{
+    res.send("My first NodeJS server!");
+})
+
+server.listen(8080)
